50f8bb6eb31ec3a86f6b88c6ed3bdebd9324a43c
Haseeb ur Rehman <haseeb.ul-rehman@n3o.ltd>
2025-12-03 16:06:55 +0500

    load remote options

--- src/filters.ts
+++ src/filters.ts
@@ -4,147 +4,10 @@ import htm from "htm";
 import Litepicker from "litepicker";
 import TomSelect from "tom-select";
 
-import type { TomTemplate, TomSettings } from "tom-select/src/types/index.ts";
+import type { TomSettings } from "tom-select/src/types/index.ts";
 import { store } from "./store";
 
 const html = htm.bind(h);
-
-type SelectProps = {
-  id?: string;
-  label: string;
-  options: {
-    label: string;
-    value: string;
-  }[];
-  path: string;
-  multiple: boolean;
-  tomSelectOpts?: TomSettings;
-  classes?: {
-    container?: string;
-  };
-};
-
-function Select(props: SelectProps) {
-  const ref = useRef<HTMLSelectElement>(null);
-  const cx = props.classes ?? {};
-
-  const renderItem: TomTemplate = (data, escape) => {
-    return "<div>" + escape(data.text) + "</div>";
-  };
-  const renderOption: TomTemplate = (data, escape) => {
-    return "<div>" + escape(data.text) + "</div>";
-  };
-
-  useEffect(() => {
-    if (ref.current) {
-      new TomSelect(ref.current, {
-        allowEmptyOption: true,
-        render: {
-          item: renderItem,
-          option: renderOption,
-        },
-        ...props.tomSelectOpts,
-      });
-    }
-  }, []);
-
-  return html`
-    <div class="${cx.container}">
-      <div class="form-label">${props.label}</div>
-      <select
-        ref=${ref}
-        class="form-select"
-        id=""
-        data-path="${props.path}"
-        multiple=${props.multiple}
-      >
-        ${props.options.map(
-          (item) =>
-            html`<option key=${item.value} value=${item.value}>
-              ${item.label}
-            </option>`,
-        )}
-      </select>
-    </div>
-  `;
-}
-
-type DateRangePickerProps = {
-  name: string;
-  label: string;
-  classes?: {
-    container?: string;
-  };
-};
-
-function DateRangePicker(props: DateRangePickerProps) {
-  const [show, setShow] = useState(false);
-  const pickerRef = useRef<Litepicker>();
-  const cx = props.classes ?? {};
-
-  useEffect(() => {
-    const startElem = document.querySelector<HTMLElement>(
-      "#datepicker-range-start",
-    );
-    const endElem = document.querySelector<HTMLElement>(
-      "#datepicker-range-end",
-    );
-
-    if (startElem && endElem) {
-      console.log(pickerRef.current);
-      pickerRef.current = new Litepicker({
-        element: startElem,
-        elementEnd: endElem,
-        singleMode: false,
-      });
-    }
-    console.log(pickerRef.current);
-  }, []);
-
-  return html`
-    <div class="${cx.container}">
-      <${Select}
-        label="Date Range"
-        options=${[
-          { value: "", label: "None" },
-          { value: "x", label: "Custom Dates" },
-          { value: "7", label: "Last 7 days" },
-          { value: "14", label: "Last 14 days" },
-        ]}
-        tomSelectOpts=${{
-          onChange: function (newValue: string) {
-            store.getState().updateFilter("dateRange", newValue);
-            if (newValue === "x") {
-              setShow(true);
-            } else {
-              setShow(false);
-            }
-          },
-        }}
-      />
-      <div class="form-control relative" hidden=${!show}>
-        <div class="flex align-items-center">
-          <span>Start: </span>
-          <input
-            class="col-3 border-0"
-            placeholder="Select start date"
-            id="datepicker-range-start"
-          />
-          <span class="col-1">End: </span>
-          <input
-            class="col-3 border-0"
-            placeholder="Select end date"
-            id="datepicker-range-end"
-          />
-        </div>
-        <div
-          class="picker-show"
-          onClick=${() => pickerRef.current?.show()}
-        ></div>
-      </div>
-    </div>
-  `;
-}
 
 export function Filters() {
   return html`
@@ -213,18 +76,148 @@ export function Filters() {
       </div>
       <${Select}
         label="Type"
-        options=${[
-          { value: "1", label: "Regular Donation" },
-          { value: "2", label: "Single Donation" },
-        ]}
         multiple
         classes=${{ container: "mb-2" }}
         tomSelectOpts=${{
           onChange: function (newValue: string[]) {
             store.getState().updateFilter("income.types", newValue);
           },
+          options: [
+            { value: "1", text: "Regular Donation" },
+            { value: "2", text: "Single Donation" },
+          ],
         }}
       />
+    </div>
+  `;
+}
+
+type DateRangePickerProps = {
+  name: string;
+  label: string;
+  classes?: {
+    container?: string;
+  };
+};
+
+function DateRangePicker(props: DateRangePickerProps) {
+  const [show, setShow] = useState(false);
+  const pickerRef = useRef<Litepicker>();
+  const cx = props.classes ?? {};
+
+  useEffect(() => {
+    const startElem = document.querySelector<HTMLElement>(
+      "#datepicker-range-start",
+    );
+    const endElem = document.querySelector<HTMLElement>(
+      "#datepicker-range-end",
+    );
+
+    if (startElem && endElem) {
+      console.log(pickerRef.current);
+      pickerRef.current = new Litepicker({
+        element: startElem,
+        elementEnd: endElem,
+        singleMode: false,
+      });
+    }
+    console.log(pickerRef.current);
+  }, []);
+
+  return html`
+    <div class="${cx.container}">
+      <${Select}
+        label="Date Range"
+        options=${[
+          { value: "", label: "None" },
+          { value: "x", label: "Custom Dates" },
+          { value: "7", label: "Last 7 days" },
+          { value: "14", label: "Last 14 days" },
+        ]}
+        tomSelectOpts=${{
+          onChange: function (newValue: string) {
+            store.getState().updateFilter("dateRange", newValue);
+            if (newValue === "x") {
+              setShow(true);
+            } else {
+              setShow(false);
+            }
+          },
+          valueField: "url",
+          labelField: "name",
+          searchField: ["name"],
+          preload: true,
+          load: function (_query: string, callback: any) {
+            var url = "https://api.github.com/search/repositories?q=tomselect";
+            fetch(url)
+              .then((response) => response.json())
+              .then((json) => {
+                callback(json.items);
+              })
+              .catch(() => {
+                callback();
+              });
+          },
+        }}
+      />
+      <div class="form-control relative" hidden=${!show}>
+        <div class="flex align-items-center">
+          <span>Start: </span>
+          <input
+            class="col-3 border-0"
+            placeholder="Select start date"
+            id="datepicker-range-start"
+          />
+          <span class="col-1">End: </span>
+          <input
+            class="col-3 border-0"
+            placeholder="Select end date"
+            id="datepicker-range-end"
+          />
+        </div>
+        <div
+          class="picker-show"
+          onClick=${() => pickerRef.current?.show()}
+        ></div>
+      </div>
+    </div>
+  `;
+}
+
+type SelectProps = {
+  id?: string;
+  label: string;
+  path: string;
+  multiple: boolean;
+  tomSelectOpts?: TomSettings;
+  classes?: {
+    container?: string;
+  };
+};
+
+function Select(props: SelectProps) {
+  const ref = useRef<HTMLSelectElement>(null);
+  const cx = props.classes ?? {};
+
+  useEffect(() => {
+    if (ref.current) {
+      new TomSelect(ref.current, {
+        allowEmptyOption: true,
+        ...props.tomSelectOpts,
+      });
+    }
+  }, []);
+
+  return html`
+    <div class="${cx.container}">
+      <div class="form-label">${props.label}</div>
+      <select
+        ref=${ref}
+        class="form-select"
+        id="${props.id}"
+        data-path="${props.path}"
+        multiple=${props.multiple}
+      ></select>
     </div>
   `;
 }
